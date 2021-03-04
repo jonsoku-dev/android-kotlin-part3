e8c9c9d3a0b13f8113414443138cbefef3206273
jonsoku2 <whdtjr2792@gmail.com>
2021-03-04 23:50:11 +0900

    13강 AppBar Layout 을 사용하면 상단 바를 스크롤하여 움직일 수 있다.

--- app/src/main/java/com/example/chip/MainActivity.kt
+++ app/src/main/java/com/example/chip/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.chip
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        toolbar_layout.setCollapsedTitleTextColor(Color.WHITE)
+        toolbar_layout.setExpandedTitleColor(Color.GREEN)
+
+        toolbar_layout.collapsedTitleGravity = Gravity.CENTER_HORIZONTAL
+        toolbar_layout.expandedTitleGravity = Gravity.RIGHT + Gravity.TOP
+
+        imageView.setImageResource(R.drawable.imgflag8)
+
+        supportActionBar?.title = "title!"
     }
 }
