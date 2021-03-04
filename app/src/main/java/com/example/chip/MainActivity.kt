a521a5888b9dc090dbedbb5381b9178e093e8910
jonsoku2 <whdtjr2792@gmail.com>
2021-03-05 00:04:08 +0900

    14강 TabLayout 을 통해 간단히 탭을 구성할 수 있다.

--- app/src/main/java/com/example/chip/MainActivity.kt
+++ app/src/main/java/com/example/chip/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.chip
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+
+    // ViewPager2에 셋팅하기 위한 Fragment들을 가지고 있는 ArrayList
+    val fragmentList = ArrayList<Fragment>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBar(toolbar)
+
+        toolbar.setTitleTextColor(Color.WHITE)
+
+        tabs.setTabTextColors(Color.BLACK, Color.RED)
+
+        for (i in 0..9) {
+            val sub = SubFragment("$i 번째 프래그먼트")
+            fragmentList.add(sub)
+        }
+
+        val adapter1 = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return fragmentList.size
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return fragmentList[position]
+            }
+        }
+
+        pager2.adapter = adapter1
+
+        // tab 과 viewpager(fragment)를 연결한다.
+        TabLayoutMediator(tabs, pager2) { tab, position ->
+            tab.text = "탭 $position"
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/example/chip/SubFragment.kt
@@ -0,0 +1,31 @@
+package com.example.chip
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_sub.*
+
+class SubFragment : Fragment {
+
+    lateinit var title: String
+
+    constructor(title: String) {
+        this.title = title
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sub, null)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView.text = title
+    }
+}
