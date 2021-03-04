254bd03025551c7163b362701d3f829da86558e4
jonsoku2 <whdtjr2792@gmail.com>
2021-03-04 23:02:01 +0900

    11강 Toolbar도 ActionBar 와 동일한 방법으로 네비게이션처리를 할 수 있다.

--- app/src/main/java/com/example/chip/MainActivity.kt
+++ app/src/main/java/com/example/chip/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.chip
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        button.setOnClickListener {
+            val secondIntent = Intent(this, SecondActivity::class.java)
+            startActivity(secondIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/chip/SecondActivity.kt
@@ -0,0 +1,28 @@
+package com.example.chip
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        setSupportActionBar(toolbar2)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
