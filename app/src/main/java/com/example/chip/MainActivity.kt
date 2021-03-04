02a999eb670fd41b3f2bf3109e3d657f6fdd19f3
jonsoku2 <whdtjr2792@gmail.com>
2021-03-04 23:18:43 +0900

    12강 ViewPager2를 사용하면 Fragement를 좌우로 전환할 수 있다.

--- app/src/main/java/com/example/chip/MainActivity.kt
+++ app/src/main/java/com/example/chip/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.chip
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+
+    val frag1 = SubFragment1()
+    val frag2 = SubFragment2()
+    val frag3 = SubFragment3()
+
+    val fragList = arrayOf(frag1, frag2, frag3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBar(toolbar)
+
+        val adapter1 = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return fragList.size
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return fragList[position]
+            }
+        }
+
+        pager2.adapter = adapter1
     }
 }
