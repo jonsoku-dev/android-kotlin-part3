aea93a72b30c7866760d90acbd6b50e439bf3343
jonsoku2 <whdtjr2792@gmail.com>
2021-03-04 19:04:59 +0900

    ActionBar의 기초

--- app/src/main/java/com/example/chip/MainActivity.kt
+++ app/src/main/java/com/example/chip/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.chip
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item1 -> {
+                textView.text = "첫번째 메뉴"
+            }
+
+            R.id.item2 -> {
+                textView.text = "두번째 메뉴"
+            }
+
+            R.id.item3 -> {
+                textView.text = "세번째 메뉴"
+            }
+
+            R.id.item4 -> {
+                textView.text = "네번째 메뉴"
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
